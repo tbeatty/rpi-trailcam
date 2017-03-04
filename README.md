49e1ada806516851c03017a165592997827fc0c9
Tim Beatty <tfbeatty@gmail.com>
2017-03-04 19:20:24 -0400

    Refactored code

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y python-dev python-gpiozero gpac
-wget https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
-sudo pip install picamera
+# Raspberry Pi Trailcam
+
+## Setup instructions
+
+1. Install dependencies
+
+  - `sudo apt-get update && sudo apt-get upgrade -y`
+  - `sudo apt-get install -y python-dev python-gpiozero gpac`
+
+2. Install Picamera
+
+  - `wget https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py`
+  - `sudo pip install picamera`
+
+3. Clone the repository
+
+  - `git clone https://github.com/tbeatty/rpi-trailcam`
+
+4. Start the program
+
+  - `python trailcam.py`
+
+## Usage
+
+```
+usage: trailcam.py [-h] [--output-dir OUTPUT_DIR] [--log-dir LOG_DIR]
+                   [--sharpness SHARPNESS] [--contrast CONTRAST]
+                   [--brightness BRIGHTNESS] [--saturation SATURATION]
+                   [--iso ISO] [--stabilize] [--hflip] [--vflip]
+                   [--record-secs RECORD_SECS] [--annotate-text ANNOTATE_TEXT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --output-dir OUTPUT_DIR, -o OUTPUT_DIR
+  --log-dir LOG_DIR, -l LOG_DIR
+  --sharpness SHARPNESS
+  --contrast CONTRAST
+  --brightness BRIGHTNESS
+  --saturation SATURATION
+  --iso ISO
+  --stabilize
+  --hflip
+  --vflip
+  --record-secs RECORD_SECS
+  --annotate-text ANNOTATE_TEXT
+```
+
+## Program output
+
+```
+2017-03-04 19:09:50,115 [INFO] Trailcam: Initializing
+2017-03-04 19:09:50,573 [INFO] Trailcam: Starting trailcam
+2017-03-04 19:09:58,510 [INFO] Trailcam: Recording video
+AVC-H264 import - frame size 1024 x 768 at 25.000 FPS
+AVC Import results: 900 samples - Slices: 15 I 885 P 0 B - 0 SEI - 15 IDR
+2017-03-04 19:11:14,715 [INFO] Trailcam: Exiting
+```
--- trailcam.py
+++ trailcam.py
@@ -16,18 +16,43 @@ MOTION_SENSOR_PIN = 17
 LOG = logging.getLogger('Trailcam')
 
 
-def init_logging(log_dir=None):
-  root_logger = logging.getLogger('')
-  root_logger.setLevel(logging.DEBUG)
-  formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
-  console_handler = logging.StreamHandler(sys.stdout)
-  console_handler.setFormatter(formatter)
-  root_logger.addHandler(console_handler)
-  if log_dir:
-    log_file = os.path.join(log_dir, 'trailcam.log')
-    file_handler = TimedRotatingFileHandler(log_file, when='M', backupCount=7)
-    file_handler.setFormatter(formatter)
-    root_logger.addHandler(file_handler)
+class TrailCam():
+
+  def __init__(self, camera, motion_sensor, output_dir,
+               record_secs=30, annotate_text='Trailcam'):
+    self.camera = camera
+    self.motion_sensor = motion_sensor
+    self.output_dir = output_dir
+    self.record_secs = record_secs
+    self.annotate_text = annotate_text
+    self.shutdown_requested = False
+
+  def run(self):
+    output_file = os.path.join(self.output_dir, 'video.h264')
+    while not self.shutdown_requested:
+      self.motion_sensor.wait_for_motion(5.0)
+      while self.motion_sensor.motion_detected:
+        LOG.info('Recording video')
+        self.camera.start_recording(output_file)
+        start_time = datetime.now()
+        while (datetime.now() - start_time).seconds < self.record_secs:
+          self.camera.annotate_text = '%s %s' % (
+              self.annotate_text, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+          self.camera.wait_recording(0.2)
+        self.camera.stop_recording()
+        LOG.info('Recording stopped')
+        sleep(0.5)
+        timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        encoded_file = os.path.join(self.output_dir, '%s.mp4' % timestamp)
+        LOG.info('Encoding file ' + encoded_file)
+        encode_mp4(output_file, encoded_file)
+
+  def request_shutdown(self):
+    self.shutdown_requested = True
+
+
+def encode_mp4(input_file, output_file):
+  call(['MP4Box', '-add', input_file, output_file])
 
 
 def init_camera(sharpness=0, contrast=0, brightness=0, saturation=0, iso=0,
@@ -46,6 +71,20 @@ def init_camera(sharpness=0, contrast=0, brightness=0, saturation=0, iso=0,
   return camera
 
 
+def init_logging(log_dir=None):
+  root_logger = logging.getLogger('')
+  root_logger.setLevel(logging.DEBUG)
+  formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+  console_handler = logging.StreamHandler(sys.stdout)
+  console_handler.setFormatter(formatter)
+  root_logger.addHandler(console_handler)
+  if log_dir:
+    log_file = os.path.join(log_dir, 'trailcam.log')
+    file_handler = TimedRotatingFileHandler(log_file, when='D', backupCount=7)
+    file_handler.setFormatter(formatter)
+    root_logger.addHandler(file_handler)
+
+
 def parse_args():
   parser = argparse.ArgumentParser()
   parser.add_argument('--output-dir', '-o', dest='output_dir', default='/home/pi')
@@ -58,10 +97,12 @@ def parse_args():
   parser.add_argument('--stabilize', action='store_true')
   parser.add_argument('--hflip', action='store_true')
   parser.add_argument('--vflip', action='store_true')
+  parser.add_argument('--record-secs', type=int, default=30)
+  parser.add_argument('--annotate-text', dest='annotate_text', default='Trailcam')
   return parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
   args = parse_args()
   init_logging(log_dir=args.log_dir)
   LOG.info('Initializing')
@@ -75,32 +116,30 @@ if __name__ == '__main__':
       hflip=args.hflip,
       vflip=args.vflip)
   motion_sensor = MotionSensor(MOTION_SENSOR_PIN)
-  output_file = os.path.join(args.output_dir, 'video.h264')
-  shutdown_requested = False
+  LOG.info('Starting trailcam')
+  trailcam = TrailCam(
+      camera,
+      motion_sensor,
+      args.output_dir,
+      record_secs=args.record_secs,
+      annotate_text=args.annotate_text)
 
-  def shutdown_handler():
+  def shutdown_handler(signum, frame):
     LOG.info('Signaling shutdown')
-    shutdown_requested = True
+    trailcam.request_shutdown()
 
-  while not shutdown_requested:
-    LOG.info('Waiting for motion')
-    motion_sensor.wait_for_motion()
-    LOG.info('Motion detected')
-    while motion_sensor.motion_detected:
-      LOG.info('Recording video')
-      camera.start_recording(output_file)
-      start_time = datetime.now()
-      while (datetime.now() - start_time).seconds < 30:
-        camera.annotate_text = 'Trailcam %s' % (
-            datetime.now().strftime('%d-%m-%y %H:%M:%S'))
-        camera.wait_recording(0.2)
-      camera.stop_recording()
-      sleep(0.5)
-      timestamp = datetime.now().strftime('%d-%m-%y_%H-%M-%S')
-      encoded_file = os.path.join(args.output_dir, '%s.mp4' % timestamp)
-      call(['MP4Box', '-add', output_file, encoded_file])
-    LOG.info('Motion ended')
+  signal.signal(signal.SIGTERM, shutdown_handler)
+  signal.signal(signal.SIGHUP, shutdown_handler)
 
-  camera.close()
+  try:
+    trailcam.run()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    camera.close()
+
   LOG.info('Exiting')
 
+
+if __name__ == '__main__':
+  main()
